e2901a702b4bcd78b76033a66a5511c298533639
YekWorld <97131437+YekWorld@users.noreply.github.com>
2022-05-26 16:29:28 -0700

    Updated anagram.py allow users to enter inputs

--- anagram.py
+++ anagram.py
@@ -1,14 +1,19 @@
 #CONFIRMING ANAGRAM
-def confirmAnagram(firstWord, secondWord):
+def confirmAnagram():
+    print("Input two words and check if they are anagrams")
+    firstWord = input("Enter your first word: \n")
+    secondWord = input("Enter your second word: \n")
     if (len(firstWord) == len(secondWord)):
         string1 = sorted(firstWord)
         string2 = sorted(secondWord)
 
         if (string1 == string2):
             print('True') # This is going to return True, if the condition is met
+            print("%s and %s" % (firstWord, secondWord) + " are anagrams \n")
         else:
-            print("False") # This is going to return False, if the condition is met
+            print("False") # This is going to return False, if the condition is not met
+            print("%s and %s" % (firstWord, secondWord) + " are not anagrams \n")
     else:
-        print( "%s and %s" % (firstWord, secondWord) + " are not anagram \n" )
+        print( "Wrong input..... Number of inputed letters are not the same" )
 
-confirmAnagram("secure", "rescue")
+confirmAnagram()
